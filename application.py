30f6da416deb0984b1cda2faf2a2bbe2dcafad55
youpengfei <youpengfei@letv.com>
2015-09-07 18:12:25 +0800

    新增项目和需求对应关系

--- application.py
+++ application.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 
+MAVEN_BIN = '/usr/local/bin/mvn'
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -26,16 +28,22 @@ def index():
     return render_template('index.html', all_requirement=all_requirement)
 
 
-@app.route('/build')
-def build_project():
+@app.route('/build/<int:id>')
+def build_project(id):
     timestamp = str(int(time.time()))
-    call(["rm", "-rf", "gpc"])
-    call(['git', 'clone', 'git@git.letv.cn:cloud_vod_j/gpc.git'])
-    call(['git', '-C', 'gpc', 'checkout', 'dev'])
+    requirement = db_session.query(Requirement).filter_by(id=id).one()
+    project = db_session.query(Project).filter_by(id=requirement.project_id).one()
+    call(["rm", "-rf", project.project_dir])
+    call(['git', 'clone', project.repo])
+    call(['git', '-C', project.project_dir, 'checkout', requirement.branch_name])
+    maven_result = Popen(
+        [MAVEN_BIN, '-U', 'clean', 'package', '-Dmaven.test.skip=true', '-s', '%s/settings.xml' % project.project_dir,
+         '-f', project.project_dir], stdout=subprocess.PIPE)
+    build_log = 'build.%s.log' % timestamp
+    file = open(build_log, 'w')
+    file.write(maven_result.stdout.read())
+    file.close()
 
-    build_log = "build." + timestamp + ".log"
-
-    call(['sh', 'build.sh'])
     return timestamp
 
 
@@ -50,15 +58,19 @@ def build_log(timestamp=None):
     return render_template("build_log.html", logs=list_of_all_the_lines)
 
 
-@app.route('/deploy')
-def deploy():
-    package_name = 'gpc-0.0.1-SNAPSHOT.jar'
-    ssh_key = '/home/tomcat/.ssh/id_rsa'
+@app.route('/deploy/<int:id>')
+def deploy(id):
+    requirement = db_session.query(Requirement).filter_by(id=id).one()
+    project = db_session.query(Project).filter_by(id=requirement.project_id).one()
+    server_ids = requirement.server_list.split(',')
+    package_name = project.deploy_name
     result = ""
-    if trans_data('10.154.29.54', ssh_key, '/home/tomcat/gpc/%s' % package_name, 'gpc/target/%s' % package_name):
-        result = "54发布成功"
-    if trans_data('10.154.29.53', ssh_key, '/home/tomcat/gpc/%s' % package_name, 'gpc/target/%s' % package_name):
-        result += "\r53发布成功"
+    for server_id in server_ids:
+        server = db_session.query(Server).filter_by(id=server_id)
+        ssh_key = server.key_file
+        result += trans_data(server.ip, ssh_key, server.deploy_dir + "/" + package_name,
+                             project.project_dir + '/target/%s' % package_name)
+
     return result
 
 
@@ -125,7 +137,7 @@ def requirement_add():
         return render_template("requirement_add.html", projects=projects, servers=servers)
     elif request.method == 'POST':
         branch_name = request.form['branch_name']
-        servers = request.form['servers']
+        servers = request.form.getlist('servers')
         server_list = ",".join(servers)
         project_id = request.form['project_id']
         requirement = Requirement(branch_name=branch_name, server_list=server_list, project_id=project_id)
@@ -136,4 +148,4 @@ def requirement_add():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source ~/.bash_profile
-mvn -U clean package -Dmaven.test.skip=true -s gpc/settings.xml -f gpc >> build.log
