21dc477ec0c079f44669f64648a4399c27c8c715
youpengfei <youpengfei@letv.com>
2015-09-15 12:17:38 +0800

    切换数据库到mysql

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'summer.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:nishishi@10.154.29.54/summer'
