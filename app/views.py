dee32da8301714eee34a32dfe2854a570322d5ce
youpengfei <youpengfei@letv.com>
2015-09-17 18:51:11 +0800

    新增日志打印sql

--- app/views.py
+++ app/views.py
@@ -78,17 +78,17 @@ def init_project(id):
     for server_id in server_ids:
         server = Server.query.filter_by(id=server_id).one()
         ssh_key = server.key_file
-        print project.start_sh
-        print project.stop_sh
 
         command(server.ip, ssh_key, 'mkdir -p %s' % project.deploy_dir)
 
         command(server.ip, ssh_key, 'touch   %s/%s' % (project.deploy_dir, 'start_for_summer.sh'))
-        command(server.ip, ssh_key, 'echo  "`%s`" > %s/%s' % (project.start_sh, project.deploy_dir, 'start_for_summer.sh'))
+        command(server.ip, ssh_key,
+                'echo  "%s" > %s/%s' % (project.start_sh.replace('$', '\$'), project.deploy_dir, 'start_for_summer.sh'))
         command(server.ip, ssh_key, 'chmod u+x   %s/%s' % (project.deploy_dir, 'start_for_summer.sh'))
 
         command(server.ip, ssh_key, 'touch   %s/%s' % (project.deploy_dir, 'stop.sh'))
-        command(server.ip, ssh_key, 'echo  "`%s`" >%s/%s' % (project.stop_sh, project.deploy_dir, 'stop.sh'))
+        command(server.ip, ssh_key,
+                'echo  "%s" >%s/%s' % (project.stop_sh.replace('$', '\$'), project.deploy_dir, 'stop.sh'))
         command(server.ip, ssh_key, 'chmod u+x   %s/%s' % (project.deploy_dir, 'stop.sh'))
 
     return result
