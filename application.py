45d63af1232814e280c91a9c50e8ecb8dd4a6066
youpengfei <youpengfei@letv.com>
2015-09-17 17:52:46 +0800

    新增

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 #!summer/bin/python
+import sys
 from app import app, db
 
 if __name__ == '__main__':
+    sys.setdefaultencoding('utf-8')
     db.create_all()
     app.run(host='0.0.0.0', debug=True)
