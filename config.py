320764a683402942378826a89516cab4343ad0bd
youpengfei <youpengfei@letv.com>
2015-09-17 18:30:19 +0800

    新增日志打印sql

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:nishishi@10.154.29.54/summer'
+SQLALCHEMY_ECHO = True
